18fc8f49cb5084a1573472277cc4b9a9b55cbfdc
wangyanhui <1011048154@qq.com>
2020-03-01 15:05:17 +0800

    修改不显示分数的问题

--- src/components/autoXunHuan.vue
+++ src/components/autoXunHuan.vue
@@ -50,16 +50,16 @@
             </div>
         </div> -->
 
-        
-        <!-- <div v-show="showData" v-html="getScore">
-       
-       </div> -->
-       <div>
+          <div>
           {{UserName}}
        </div>
        <div>
           {{UserNum}}
        </div>
+        <div v-show="showData" v-html="getScore">
+       
+       </div>
+     
     </div>
 </template>
 
@@ -92,9 +92,7 @@
         },
         computed: {
             getTongGuan(){
-                console.log(this)
-                console.log(this.$store)
-                console.log(this.$store.state)
+              
                 let a = this.$store.state.dianGong3
                 return a;
             },
@@ -115,9 +113,7 @@
                 // console.log(this.qianGong3);
                 let _this = this;
                //  _this.whichCard = _this.zhaGangTiZhengFen[9]
-                console.log(_this.getTongGuan);
-                console.log("this,get")
-                console.log(_this.subqu)
+                
                 _this.whichCard = _this.getTongGuan[_this.subqu];
                 // 开始选择那一关
                 _this.whichOne();
@@ -127,10 +123,9 @@
             whichOne (){
                 let _this = this;
                 axios.get("/cglb/dt?id="+_this.whichCard+"&matchid=0").then( (res) => {
-                    console.log("------开始选择那一关-----");
-                    console.log(res)
+              
                     _this.iframeContent.push(res.data);
-                    console.log((_this.iframeContent[0].indexOf("该关口未解锁")))
+                  
                     // 设置自动找合适的关卡
                     if(_this.iframeContent[0].indexOf("该关口未解锁") != -1) {
                        _this.subqu++;
@@ -147,7 +142,7 @@
             sendFirst () {
                 let _this = this;
                 axios.get("/cglb/ajaxdt?passid="+_this.whichCard+"&id=0&answer=0").then( (res) => {
-                    console.log(res)
+                   
                     _this.sendRight() // 开始答题
                 })
             },
@@ -159,7 +154,7 @@
                     var aNum = setTimeout( () => {
                        // if(_this.yiChangCtr){ // 发生异常情况
                           // 异常情况计数
-                          console.log("异常触发了。。。。")
+                          
                           _this.yiChangNum ++;
                           _this.$router.go(0);
 
@@ -210,7 +205,7 @@
                         // }
                        axios.get("/cglb/ajaxdt?passid="+_this.whichCard+"&id="+answer27101[randomNum].id+"&answer="+answer27101[randomNum].answer).then( (res) => {
                         
-                        console.log(res.data)
+                       
                            
                            for(let i=0;i<9999;i++){
                               clearTimeout(i)
@@ -241,8 +236,7 @@
             getRightData () {
                let _this = this;
                axios.get("/cglb/passresult?passid="+_this.whichCard+"mathchid=0").then( (res) => {
-                   console.log("------获取答题结果数据------");
-                   console.log(res);
+                   
                    // let  reg = /(?<=\<ul\>).+(?=\<\\ul\>)/;
                    // _this.iframe0 = res.data.match(reg);
                    // _this.iframe0 = res.data;
@@ -281,27 +275,30 @@
             })
 
              setTimeout( () => {
-                 console.log("执行了这句....")
+                
                  try {
                      var li0 = document.getElementsByTagName("script");
                   
                     console.log(li0)
                     console.log(111111)
                     var li = document.getElementsByTagName("script")[11].text;
+                    console.log(li)
                     if(li == ""){
                          li = document.getElementsByTagName("script")[12].text;
                     }
                     var echarts={};
                     var option;
                     var myChart = {} ; 
+                    console.log("....****")
                     myChart.setOption = function(){
-                        console.log(0)
+                        console.log(0) //
                     }
                     echarts.init = function(){
                     return {setOption:function(pram){
+                        console.log("....")
                         console.log(pram) // 所有的script标签
                         console.log(pram.series) // 获得多少的分数
-                        _this.HadScore = pram.series[0].data[1]
+                        _this.HadScore = pram.series[0].data[2]
                         let a;
                         a = sessionStorage.getItem("willScore");
                         if(Number(a) < Number(_this.HadScore)){
@@ -333,10 +330,10 @@
             
         },
         beforeUpdate(){
-            console.log("数据更新之前")
+           // console.log("数据更新之前")
         },
         updated(){
-            console.log("数据更新完成....")
+           //  console.log("数据更新完成....")
         },
         beforeDestroy() {
             for(let i=0;i<9999;i++){
--- src/components/cookie.js
+++ src/components/cookie.js
@@ -4,8 +4,10 @@ let getCookie ={
     c:''
 } ;
 getCookie.a = "PHPSESSID=lktqs8ip29sginercq6c9cukh0;;"   
-getCookie.b = "BJ83T0U0lFrKq2=27032;"
-getCookie.c = "safetycode2=9165"
+getCookie.b = "BJ83T0U0lFrKq1=34320;"
+getCookie.c = "safetycode1=3265"
 export default getCookie;
 
-// BJ83T0U0lFrKq1=21025; safetycode1=3637; 
+//  BJ83T0U0lFrKq1=34322; safetycode1=5429; 
+// BJ83T0U0lFrKq1=33927; safetycode1=6264; 
+//  BJ83T0U0lFrKq1=34320; safetycode1=3265; 
