c7a11f98d9242e71d7a395db0a1014c61436a730
wangyanhui <1011048154@qq.com>
2020-03-07 14:02:30 +0800

    切换dev

--- src/components/cookie.js
+++ src/components/cookie.js
@@ -1,9 +1,6 @@
 let getCookie ={} ;
 getCookie.a = "PHPSESSID=n0oh8ithjv98ca6nkc4c36pbj1;"   
-getCookie.b = "BJ83T0U0lFrKq1=21544;"
-getCookie.c = "safetycode1=2354"
+getCookie.b = "BJ83T0U0lFrKq2=27278;"
+getCookie.c = "safetycode2=9215"
 export default getCookie;
-// "PHPSESSID=ssm53st9gcj8sq1npmukvgstg0; BJ83T0U0lFrKq1=20827; safetycode1=3873; 
-// "PHPSESSID=n0oh8ithjv98ca6nkc4c36pbj1; BJ83T0U0lFrKq1=20827; safetycode1=2389; 
-  // BJ83T0U0lFrKq1=21544; safetycode1=2354; 
-  //  BJ83T0U0lFrKq1=20929; safetycode1=9569"
+//  BJ83T0U0lFrKq2=27278; safetycode2=9215;
