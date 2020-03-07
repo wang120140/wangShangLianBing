e0000fa6d9f4622a75b4098300720ceb7354edb6
wangyanhui <1011048154@qq.com>
2020-03-07 14:06:31 +0800

    修改部分文件

--- src/components/cookie.js
+++ src/components/cookie.js
@@ -4,9 +4,10 @@ let getCookie ={
     c:''
 } ;
 getCookie.a = "PHPSESSID=lktqs8ip29sginercq6c9cukh0;;"   
-getCookie.b = "BJ83T0U0lFrKq1=21544;"
-getCookie.c = "safetycode1=2354"
+getCookie.b = "BJ83T0U0lFrKq2=27278;"
+getCookie.c = "safetycode2=9215"
 export default getCookie;
 
 //  BJ83T0U0lFrKq1=20973; safetycode1=3097;
 // BJ83T0U0lFrKq1=21544; safetycode1=2354"
+// BJ83T0U0lFrKq2=27278; safetycode2=9215;
